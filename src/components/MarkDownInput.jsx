e2f349309be83563fd56425399c515d7489728f0
thomas.bobichon@hotmail.fr <thomas.bobichon@hotmail.fr>
2024-10-23 09:40:16 +0200

    Visual Improv

--- src/components/MarkDownInput.jsx
+++ src/components/MarkDownInput.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import "../styles/main.scss";
 
 const MarkdownInput = ({ note, onContentChange, onTitleChange }) => {
   return (
     <div>
       <input
+        className="note-title-input"
         type="text"
         placeholder="Titre de la note"
         value={note.title}
         onChange={(e) => onTitleChange(e.target.value)}
       />
       <textarea
+        className="note-content-textarea"
         value={note.content}
         onChange={(e) => onContentChange(e.target.value)}
         placeholder="Écris ta note en Markdown ici..."
